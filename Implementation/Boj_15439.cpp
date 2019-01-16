47eda76a41bf0e1450ac4196b3317ba3a5f76649
b8goal <b8goal@naver.com>
2019-01-16 19:54:49 +0900

    2019.01.16

---
+++ Implementation/Boj_15439.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <map>
+#include <stack>
+#define C 10
+using namespace std;
+
+typedef long long ll;
+const int INF = 100000000;
+const int MOD = 1000000007;
+const int MAXN = 4000;
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	printf("%d\n", n*(n - 1));
+	return 0;
+}
