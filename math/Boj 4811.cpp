0bf0db853932f57677376104f955d557b120464e
b8goal <b8goal@naver.com>
2019-01-28 15:42:49 +0900

    카탈란수

---
+++ math/Boj 4811.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <cmath>
+typedef long long ll;
+using namespace std;
+const int MOD = 9901;
+const int INF = 0x7fffffff;
+const int MAXN = 300;
+int p[1000];
+ll cache[31] = { 0, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012, 742900, 2674440, 9694845, 35357670, 129644790, 477638700, 1767263190, 6564120420, 24466267020, 91482563640, 343059613650, 1289904147324, 4861946401452, 18367353072152, 69533550916004, 263747951750360, 1002242216651368, 3814986502092304 };
+int find(int x) { return p[x] ^ x ? find(p[x]) : x; }
+int abs(int x, int y) { return x - y > 0 ? x - y : y - x; }
+int gcd(int x, int y) { return y ? gcd(y, x%y) : x; }
+long long i, w, sw, sum, l;
+int main(void) {
+	while (1) {
+		int x;
+		scanf("%d", &x);
+		if (!x) break;
+		printf("%lld\n", cache[x]);
+	}
+	
+	return 0;
+}
+
