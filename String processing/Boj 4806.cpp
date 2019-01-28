eecad34af96f453390ca77a8cf8ca49d3801519e
b8goal <b8goal@naver.com>
2019-01-28 15:10:55 +0900

    등비, 등차

---
+++ String processing/Boj 4806.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+typedef long long ll;
+using namespace std;
+const int MOD = 9901;
+const int INF = 0x7fffffff;
+const int MAXN = 300;
+int p[1000], A[100001];
+int find(int x) { return p[x] ^ x ? find(p[x]) : x; }
+int abs(int x, int y) { return x - y > 0 ? x - y : y - x; }
+int gcd(int x, int y) { return y ? gcd(y, x%y) : x; }
+long long i, w, sw, sum, l;
+int main(void) {
+	string s;
+	int num = 0;
+	while (getline(cin, s)) {
+		++num;
+	}
+	cout << num << '\n';
+	return 0;
+}
+
