00c02c16ac9b67e050e71385ad0394e18e28730f
b8goal <b8goal@naver.com>
2019-02-01 10:03:20 +0900

    2019_02_01

---
+++ String processing/Boj 16755.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+typedef long long ll;
+using namespace std;
+const int MOD = 1000000;
+const int INF = 0x7fffffff;
+const int MAXN = 300;
+int p[1000001], dp[1001][1001];
+string s[50];
+ll cache[61][61];
+int find(int x) { return p[x] ^ x ? find(p[x]) : x; }
+void merge(int x, int y) { p[find(y)] = find(x); }
+int abs(int x, int y) { return x - y > 0 ? x - y : y - x; }
+int gcd(int x, int y) { return y ? gcd(y, x%y) : x; }
+int min(int x, int y) { return x > y ? y : x; }
+int max(int x, int y) { return x > y ? x : y; }
+
+char honi[] = "HONI";
+int main(void) {
+	string s;
+	cin >> s;
+	int k = 0, cnt = 0;
+	for (int i = 0; s[i]; ++i) {
+		if (s[i] == honi[k]) ++k;
+		if (k == 4) {
+			k = 0;
+			++cnt;
+		}
+	}
+	cout << cnt << '\n';
+	return 0;
+}
