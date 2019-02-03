9322b157d5c5b076a12aa0493421611ec276ea48
b8goal <b8goal@naver.com>
2019-02-04 05:06:24 +0900

    list

---
+++ Ad Hoc/Boj 5524.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+#include <list>
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define REP(i,n) FOR(i,0,n) 
+typedef unsigned long long ull;
+typedef long long ll;
+const int MOD = 1e9 + 7;
+const int INF = 0x7fffffff;
+const int MAXN = 500;
+using namespace std;
+int dx[4] = { 1,-1,0,0 }, dy[4] = { 0,0,1,-1 };
+int p[100], a[MAXN + 1], sum[MAXN+1], cache[MAXN + 1][MAXN + 1];
+int find(int x) { return p[x] ^ x ? find(p[x]) : x; }
+void merge(int x, int y) { p[find(y)] = find(x); }
+int abs(int x, int y) { return x - y > 0 ? x - y : y - x; }
+int gcd(int x, int y) { return y ? gcd(y, x%y) : x; }
+int min(int x, int y) { return x > y ? y : x; }
+int max(int x, int y) { return x > y ? x : y; }
+int n, m, x, y;
+
+int main(void) {
+	int tc;
+	cin >> tc;
+	REP(i, tc) {
+		string s;
+		cin >> s;
+		REP(i, s[i]) {
+			if ('A' <= s[i] && s[i] <= 'Z')
+				cout << (char)(s[i] - 'A' + 'a');
+			else
+				cout << s[i];
+		}
+		puts("");
+	}
+	return 0;
+}
