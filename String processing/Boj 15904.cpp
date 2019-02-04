09ce12a19df4417a4ec60164b33788f5673a2264
b8goal <b8goal@naver.com>
2019-02-04 20:34:54 +0900

    UCPC

---
+++ String processing/Boj 15904.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+#include <list>
+#include <bitset>
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define REP(i,n) FOR(i,0,n) 
+typedef unsigned long long ull;
+typedef long long ll;
+const int MOD = 1e9 + 7;
+const int INF = 0x7fffffff;
+const int MAXN = 100000 + 1;
+using namespace std;
+int p[100];
+int find(int x) { return p[x] ^ x ? find(p[x]) : x; }
+void merge(int x, int y) { p[find(y)] = find(x); }
+int abs(int x, int y) { return x - y > 0 ? x - y : y - x; }
+int gcd(int x, int y) { return y ? gcd(y, x%y) : x; }
+int min(int x, int y) { return x > y ? y : x; }
+ll max(ll x, ll y) { return x > y ? x : y; }
+int pow(int x, int y) {
+	if (y == 0) return 0;
+	if (y == 1) return x;
+	if (y % 2 == 0) {
+		int ret = pow(x, y / 2);
+		return ret = (ret * ret);
+	}
+	else {
+		int ret = pow(x, (y - 1) / 2);
+		return ret = ret * ret*x;
+	}
+}
+ll a[MAXN];
+int cnt;
+
+int main(void) {
+	string s;
+	getline(cin, s);
+	for (int i=0; i < s.size(); i++) {
+		if (!cnt && s[i] == 'U')
+			++cnt;
+		else if (cnt == 1 && s[i] == 'C')
+			++cnt;
+		else if (cnt == 2 && s[i] == 'P')
+			++cnt;
+		else if (cnt == 3 && s[i] == 'C')
+			++cnt;
+	}
+	if (cnt == 4)
+		puts("I love UCPC");
+	else
+		puts("I hate UCPC");
+	return 0;
+}
