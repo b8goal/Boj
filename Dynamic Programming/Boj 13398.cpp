14bb71efebb372f15621ce46e0dc0ac74abe4ba9
b8goal <b8goal@naver.com>
2019-02-24 01:43:36 +0900

    연속합2

---
+++ Dynamic Programming/Boj 13398.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+#include <list>
+#include <bitset>
+#include <random>
+#include <ctime>
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define REP(i,n) FOR(i,0,n)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<string, int> psi;
+const int MOD = 10007;
+const int INF = 0x7fffffff;
+const int MIN = -32768 * 101;
+const int MAXN = 100000 + 1;
+const long double PI = 3.141592653589793238462643383279502884197;
+int p[100];
+int find(int x) { return p[x] ^ x ? find(p[x]) : x; }
+void merge(int x, int y) { p[find(y)] = find(x); }
+double abs(double x, double y) { return x - y > 0 ? x - y : y - x; }
+int gcd(int x, int y) { return y ? gcd(y, x%y) : x; }
+
+int a[100001], cache[100001][2]; // 0 -> 제거 X , 1 -> 제거 
+int main(void) {
+	int n, mx;
+	scanf("%d", &n);
+	REP(i, n)scanf("%d", &a[i]), cache[i][0] = a[i];
+	cache[0][0] = a[0];
+	mx = cache[0][0];
+	FOR(i, 1, n) {
+		cache[i][0] = max(cache[i - 1][0] + a[i], a[i]);
+		cache[i][1] = max(cache[i-1][0], cache[i-1][1] + a[i]);
+		mx = max({ mx,cache[i][0], cache[i][1] });
+	}
+	printf("%d\n", mx);
+	return 0;
+}
