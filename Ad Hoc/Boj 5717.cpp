bc2fc23d705f2095b252327b54406af976d89a9a
b8goal <b8goal@naver.com>
2019-04-07 23:36:03 +0900

    상근이와 친구들

---
+++ Ad Hoc/Boj 5717.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define INF 987654321
+using namespace std;
+
+int main(void) {
+	while (1) {
+		int a, b;
+		cin >> a >> b;
+		if (!(a + b))
+			break;
+		else
+			cout << a + b << '\n';
+	}
+	return 0;
+}
