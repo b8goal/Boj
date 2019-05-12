78ec9938cad384474274ec84b74d5ecc611a50d8
b8goal <b8goal@naver.com>
2019-05-13 07:14:54 +0900

    에디터

---
+++ List/Boj 1406.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define INF 0x7fffffff
+using namespace std;
+
+list<char> li;
+int main(void) {
+	string s;
+	cin >> s;
+	for (int i = 0; s[i]; i++)
+		li.push_back(s[i]);
+	int n;
+	char x, y;
+	cin >> n;
+	list<char>::iterator it = li.end();
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (x == 'P') {
+			cin >> y;
+			li.insert(it, y);
+		}
+		else if (x == 'L' && it != li.begin())
+			--it;
+		else if (x == 'D' && it != li.end())
+			++it;
+		else if (x == 'B' && it != li.begin()){
+			--it;
+			it = li.erase(it);
+		}
+	}
+	for (auto t : li) {
+		cout << t;
+	}
+	return 0;
+}
