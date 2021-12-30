8deb64d7eb85d4f4ec10c1ca764ab7d6d28e18ee
HyeonSeong <30991265+b8goal@users.noreply.github.com>
2021-12-30 18:09:54 +0900

    [Silver I] Title: 동전-2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 동전-2/동전-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(void)
+{
+  //cin.sync_with_stdio(false);
+  //cout.sync_with_stdio(false);
+
+  //freopen("../txt/input.txt", "r", stdin);
+  //freopen("../txt/output.txt","w",stdout);
+
+  int n,k;
+  int arr[100+5];
+  int cache[10000+5];
+  cin >> n >> k;
+  for(int i=1; i<=n; i++) {
+    cin >> arr[i];
+  }
+
+  memset(cache, -1, sizeof(cache));
+
+  cache[0] = 0;
+  for(int i=1; i<=n; i++) {
+    for(int j=0; j<=k; j++) {
+      if(j-arr[i] >= 0 && cache[j-arr[i]] != -1) {
+        if(cache[j] == -1 || cache[j] > cache[j-arr[i]] + 1)
+        cache[j] = cache[j-arr[i]] + 1;
+      }      
+    }
+  }
+  
+  cout << cache[k] <<'\n';
+
+  return 0;
+}
